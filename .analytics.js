4deca404a18ca518f6ff807e858f173848f4ed74
Jannik Wibker <jannikwibker@gmail.com>
2018-11-13 08:45:03 +0100

    now sending updates as group

--- .analytics.js
+++ .analytics.js
@@ -2,11 +2,11 @@ let recentlySent = false
 let sendQueue = []
 
 const analyse = (data, send) => {
-  if(data.path.endsWith('_sidebar.md')) return
-  if(!(data.path.endsWith('.md') || data.path === '/auth/login')) return
+  if(data.record.path.endsWith('_sidebar.md')) return
+  if(!(data.record.path.endsWith('.md') || data.path === '/auth/login')) return
 
-  if(data.path.endsWith('.md')) data.category = 'page'
-  if(data.path = '/auth/login') data.category = 'login'
+  if(data.record.path.endsWith('.md')) data.category = 'page'
+  if(data.record.path = '/auth/login') data.category = 'login'
 
   if(recentlySent) {
     sendQueue.push(data)
